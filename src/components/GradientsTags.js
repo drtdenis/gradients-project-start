12f3ba0339b52912f4473f11685c64930232c7f8
drtdenis <drouetdenis2@gmail.com>
2021-04-13 13:47:17 +0200

    suite

--- src/components/GradientsTags.js
+++ src/components/GradientsTags.js
@@ -3,10 +3,15 @@ const {tags, filter, setFilter} = props
 
   return (
     <div className="mt-3">
-      {
-        tags.sort().map((el) => (
-        <button type="button" className={el === filter ? "btn btn-sm me-2 mb-2 bg-dark text-white" : "btn btn-sm me-2 mb-2 bg-light"}
-        onClick= {() => setFilter(el)} disabled={el === filter}>{el}</button>
+      {tags.sort().map((el) => (
+        <button 
+        type="button" 
+        className={el === filter ?  "btn btn-sm me-2 mb-2 bg-light" :"btn btn-sm me-2 mb-2 bg-dark text-white" }
+        onClick= {() => setFilter(el)} 
+        key={el}
+        disabled={el === filter}>
+          {el}
+          </button>
         ))}
     </div>
   )
